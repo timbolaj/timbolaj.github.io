823e97c6fe29c0a046896d62ddb35ee4f3a9e1ee
Aaron <timbolaj@gmail.com>
2020-07-16 10:19:42 -0400

    added links and info for top page

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -4,7 +4,20 @@ import picture from '../images/profile_pic.jpg';
 export default function Summary() {
   return (
     <div>
-      <p>Test</p>
+      <div className="links">
+        <a href="https://github.com/timbolaj">Github</a> | <a href="https://linkedin.com/in/aaron-timbol-853383107/">LinkedIn</a>
+      </div>
+
+      <div className="profile-picture">
+        <img src={picture}></img>
+      </div>
+
+      <div className="text">
+        <p>Hi, I'm Aaron.</p>
+        <br></br>
+        <p>I'm a political science graduate turned software developer.</p>
+        <p>I love languages, both human and machine</p>
+      </div>
     </div>
   )
 }
