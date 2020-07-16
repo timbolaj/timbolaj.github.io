ae44b9fd5b57d38dc119b78ebd82823156dfb831
Aaron <timbolaj@gmail.com>
2020-07-16 11:31:13 -0400

    Basic structure set for summary

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,23 +1,39 @@
 import React from 'react';
 import picture from '../images/profile_pic.jpg';
+import '../Styles/Summary.scss';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Summary() {
   return (
-    <div>
-      <div className="links">
-        <a href="https://github.com/timbolaj">Github</a> | <a href="https://linkedin.com/in/aaron-timbol-853383107/">LinkedIn</a>
+    <div classname="summary">
+
+      <div className="links col-sm">
+        <a href="https://github.com/timbolaj">Github </a>|<a href="https://linkedin.com/in/aaron-timbol-853383107/"> LinkedIn</a>
       </div>
 
-      <div className="profile-picture">
-        <img src={picture}></img>
+      <div className="row profile">
+        <div className="col">
+          <div className="profile-picture">
+            <img src={picture}></img>
+          </div>
+
+          <br></br>
+
+          <div className="text">
+
+            <div className="name">
+              <p>Hi, I'm Aaron.</p>
+            </div>
+
+            <div className="description">
+              <p>I'm a political science graduate turned software developer.</p>
+              <p>I love languages, both human and machine.</p>
+            </div>
+          
+          </div>
+        </div>
       </div>
 
-      <div className="text">
-        <p>Hi, I'm Aaron.</p>
-        <br></br>
-        <p>I'm a political science graduate turned software developer.</p>
-        <p>I love languages, both human and machine</p>
-      </div>
     </div>
   )
 }
