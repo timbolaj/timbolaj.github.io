3cea19624935d8b2dea65ddc0fc93e3ce76bff59
Aaron <timbolaj@gmail.com>
2020-07-17 09:07:37 -0400

    Created a toggle state for the Profile component. Now switches from summary state to skill state

--- src/components/Profile-components/Summary.js
+++ src/components/Profile-components/Summary.js
@@ -6,7 +6,7 @@ export default function Summary() {
     <div className='Summary'>
       <h4>About Me</h4>
       <br></br>
-      <p>I graduated from the University of Toronto in 2018 majoring Political Science with minors in Spanish and Philosophy.</p>
+      <p>I graduated from the University of Toronto in 2018 majoring in Political Science with minors in Spanish and Philosophy.</p>
       <p>In 2019, I began working for a non-profit organization in Québec City called l'Arche Étoile, caring for persons with special need.</p>
       <p>I rediscovered programming through the Intro to JavaScript course on Codecademy and was hooked.</p>
       <p>Shortly thereafter, I discovered Lighthouse Labs and their Full-Stack Web Development Bootcamp.</p>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,20 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import picture from '../images/profile_pic.jpg';
 import '../Styles/Profile.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 import Summary from './Profile-components/Summary';
+import Button from '@material-ui/core/Button';
+
+const SUMMARY = 'Summary';
+const SKILLS = 'Skills';
 
 export default function Profile() {
+
+  const [mode, setMode] = useState('Summary');
+
   return (
-    <div className="summary d-flex justify-content-center vertical-center">
+    <div className="profile d-flex justify-content-center vertical-center">
+
       <div className="profile-picture">
         <img src={picture} alt="picture"></img>
       </div>
 
       <br></br>
 
-      <div class="jumbotron">
-        <Summary />
+      <div className="jumbotron">
+
+        <div className="profile-components">
+          {mode === 'Summary' && <Summary />}
+        </div>
+   
+        <div className="modes">
+          <Button
+            variant="contained"
+            color="secondary"
+            className="summary"
+            onClick={() => setMode(SUMMARY)}
+          >
+            Summary
+          </Button>
+
+          <Button
+            variant="contained"
+            color="secondary"
+            className="skills"
+            onClick={() => setMode(SKILLS)}
+          >
+            Skills
+          </Button>
+
+        </div>
+
       </div>
     </div>
   )
