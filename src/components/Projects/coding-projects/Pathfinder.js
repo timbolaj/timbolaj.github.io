05c15f4dcb31d2705cb9b5ae95cf011a9b4c2eba
Aaron <timbolaj@gmail.com>
2020-07-18 09:08:21 -0400

    Created desc for pathfinder

--- src/components/Projects/coding-projects/Pathfinder.js
+++ src/components/Projects/coding-projects/Pathfinder.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 export default function Pathfinder() {
   return (
-    <div>
-      Gif
-       Description
+    <div className="pathfinder">
+      <div>
+        Gif        
+      </div>
+
+      <div className="description">
+        <p>
+          A path-finding algorithm visualisation tool. Users can choose an algorithm and visualise
+          the algorithm's search for the end node from the start node. Users can also add obstacles on the map such as
+          user-drawn walls and weights, pre-made map, a randomly generated maze or a detour (intermediate node)
+        </p>       
+      </div>
+
     </div>
   )
 }
