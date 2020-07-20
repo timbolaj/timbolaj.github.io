0efd6553b96e8511461208ac43227175c96d59d2
Aaron <timbolaj@gmail.com>
2020-07-20 10:03:38 -0400

    Moved helper functions into a helper folder/file

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,7 @@ import Pathfinder from './Projects/coding-projects/Pathfinder';
 import Mapbook from './Projects/coding-projects/Mapbook';
 import Scheduler from './Projects/coding-projects/Scheduler';
 import ToggleMode from './Projects/ToggleMode';
-import { faBreadSlice } from '@fortawesome/free-solid-svg-icons';
+import { forward, back } from '../helpers/projects-helpers';
 
 export const PATHFINDER = 'pathfinder';
 export const MAPBOOK = 'mapbook';
@@ -14,49 +14,6 @@ export const ORDER = [PATHFINDER, MAPBOOK, SCHEDULER]
 export default function Projects() {
 
   const [projectMode, setProject] = useState(PATHFINDER)
-
-  const slideshow = () => {
-    clearInterval();
-    switch (projectMode) {
-      case PATHFINDER:
-        setProject(MAPBOOK);
-        break;
-      case MAPBOOK:
-        setProject(SCHEDULER);
-        break;
-      case SCHEDULER:
-        setProject(PATHFINDER);
-        break;
-    }
-  }
-
-  const forward = () => {
-    switch (projectMode) {
-      case PATHFINDER:
-        setProject(MAPBOOK);
-        break;
-      case MAPBOOK:
-        setProject(SCHEDULER);
-        break;
-      case SCHEDULER:
-        setProject(PATHFINDER);
-        break;
-    }
-  }
-
-  const back = () => {
-    switch (projectMode) {
-      case PATHFINDER:
-        setProject(SCHEDULER);
-        break;
-      case MAPBOOK:
-        setProject(PATHFINDER);
-        break;
-      case SCHEDULER:
-        setProject(MAPBOOK);
-        break;
-    }
-  }
 
   return (
     <div className="Projects">
---
+++ src/helpers/projects-helpers.js
@@ -0,0 +1,44 @@
+export const forward = (mode) => {
+  switch (mode) {
+    case PATHFINDER:
+      setProject(MAPBOOK);
+      break;
+    case MAPBOOK:
+      setProject(SCHEDULER);
+      break;
+    case SCHEDULER:
+      setProject(PATHFINDER);
+      break;
+  }
+}
+
+export const back = (mode) => {
+  switch (mode) {
+    case PATHFINDER:
+      setProject(SCHEDULER);
+      break;
+    case MAPBOOK:
+      setProject(PATHFINDER);
+      break;
+    case SCHEDULER:
+      setProject(MAPBOOK);
+      break;
+  }
+}
+
+/* 
+  const slideshow = () => {
+    clearInterval();
+    switch (projectMode) {
+      case PATHFINDER:
+        setProject(MAPBOOK);
+        break;
+      case MAPBOOK:
+        setProject(SCHEDULER);
+        break;
+      case SCHEDULER:
+        setProject(PATHFINDER);
+        break;
+    }
+  }
+*/
