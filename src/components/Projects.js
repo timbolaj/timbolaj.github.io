d0a59d485a35f69c94a0741f4ffbd388c747472b
Aaron <timbolaj@gmail.com>
2020-07-20 07:07:31 -0400

    made a slideshow set interval for projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,17 +4,40 @@ import Pathfinder from './Projects/coding-projects/Pathfinder';
 import Mapbook from './Projects/coding-projects/Mapbook';
 import Scheduler from './Projects/coding-projects/Scheduler';
 
+const PATHFINDER = 'pathfinder';
+const MAPBOOK = 'mapbook';
+const SCHEDULER = 'scheduler';
+//We can make a slideshow with SetInterval!;
+
 export default function Projects() {
 
+  const [projectMode, setProject] = useState(PATHFINDER)
+
+  const slideshow = () => {
+    switch (projectMode) {
+      case PATHFINDER:
+        setProject(MAPBOOK);
+        break;
+      case MAPBOOK:
+        setProject(SCHEDULER);
+        break;
+      case SCHEDULER:
+        setProject(PATHFINDER);
+        break;
+    }
+  }
+
   return (
-    <div className="Projects">
+    <div className="Projects" onpageshow={setInterval(slideshow, 10000)}>
       <h4>Coding Projects</h4>
 
       <div className="websites">
-        <Pathfinder />
-        <Mapbook />
-        <Scheduler />
+        {projectMode === PATHFINDER && <Pathfinder />}
+        {projectMode === MAPBOOK && <Mapbook />}
+        {projectMode === SCHEDULER && <Scheduler />}
       </div>
+
+      <div>Toggle bar</div>
       <br></br>
 
       <h4>Articles</h4>
